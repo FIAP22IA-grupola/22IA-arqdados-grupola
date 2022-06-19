05af5a0504bae4454d234a625805b770520562a7
Rubauliant <rubens.alasmar@gmail.com>
2022-06-19 18:27:14 -0300

    O banco sobe tudo sozinho no docker-compose

--- create_database/dump.sql
+++ create_database/dump.sql
@@ -12,115 +12,127 @@ CREATE SCHEMA IF NOT EXISTS "dindinagora"
 -- Table "dindinagora"."endereco"
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS "dindinagora"."endereco" (
-  "ID" SERIAL,
+  "id" SERIAL PRIMARY KEY,
   "logradouro" VARCHAR(255) NOT NULL,
-  "CEP" VARCHAR(255) NOT NULL,
-  "complemento" VARCHAR(255) NULL,
-  PRIMARY KEY ("ID"));
+  "cep" VARCHAR(255) NOT NULL,
+  "complemento" VARCHAR(255) NULL
+  );
   
 -- -----------------------------------------------------
 -- Table "dindinagora"."cliente"
 -- -----------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS "dindinagora"."cliente" (
-  "ID" SERIAL PRIMARY KEY,
+  "id" SERIAL PRIMARY KEY,
   "nome" VARCHAR(255) NOT NULL,
   "RG" VARCHAR(255) NOT NULL,
   "CPF" VARCHAR(255) NOT NULL,
-  "enderecoFK" INT NULL,
+  "endereco_fk" INT NOT NULL,
     CONSTRAINT "fk_cliente_endereco"
-    FOREIGN KEY ("enderecoFK")
-        REFERENCES "dindinagora"."endereco"("ID")
+    FOREIGN KEY ("endereco_fk")
+        REFERENCES "dindinagora"."endereco"("id")
         ON DELETE NO ACTION
         ON UPDATE NO ACTION
-   );
+  );
 
 CREATE INDEX "fk_cliente_endereco_idx"
-ON "dindinagora"."cliente" ("enderecoFK" ASC);
+ON "dindinagora"."cliente" ("endereco_fk" ASC);
 
 -- --------------------------------------------------
 -- Table "dindinagora"."conta"
 -- --------------------------------------------------
 -- DROP TABLE "dindinagora"."conta";
 CREATE TABLE IF NOT EXISTS "dindinagora"."conta" (
-  "ID" SERIAL,
+  "id" SERIAL PRIMARY KEY,
   "cliente_fk" INT NOT NULL,
+  "tipo" VARCHAR(255),
   "saldo" FLOAT NOT NULL,
-  PRIMARY KEY ("ID"),
-  CONSTRAINT fk_conta_cliente
-    FOREIGN KEY (cliente_fk)
-    REFERENCES "dindinagora"."cliente"("ID")
+  CONSTRAINT "fk_conta_cliente"
+  FOREIGN KEY ("cliente_fk")
+    REFERENCES "dindinagora"."cliente"("id")
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-CREATE INDEX "fk_conta_cliente_idx"
-ON "dindinagora"."conta"(cliente_fk);
+    ON UPDATE NO ACTION,
+  CONSTRAINT ckeck_tipoconta check ("tipo" in ('Conta Poup', 'Conta Corr'))
+  );
 
 CREATE UNIQUE INDEX "unique_fk_conta_cliente_idx"
-ON "dindinagora"."conta"(cliente_fk);
+ON "dindinagora"."conta"(cliente_fk,tipo);
 
 -- -----------------------------------------------------
 -- Table "dindinagora"."transferencia"
 -- -----------------------------------------------------
 -- DROP TABLE "dindinagora"."transferencia";
-CREATE TABLE IF NOT EXISTS "dindinagora"."transferencia" (
-  "ID" SERIAL,
+CREATE TABLE IF NOT EXISTS "dindinagora"."tr_transferencia" (
+  "id" SERIAL PRIMARY KEY,
   "montante" FLOAT NOT NULL,
   "tipo" CHAR(3),
+  "data_hora_transacao" TIMESTAMP NOT NULL,
   "origem_fk" INT NOT NULL,
   "destino_fk" INT NOT NULL,
-  PRIMARY KEY ("ID"),
   CONSTRAINT "fk_transferencia_origem"
     FOREIGN KEY (origem_fk)
-    REFERENCES "dindinagora"."conta"("ID")
+    REFERENCES "dindinagora"."conta"("id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT "fk_transferencia_destino"
     FOREIGN KEY (destino_fk)
-    REFERENCES "dindinagora"."conta"("ID")
+    REFERENCES "dindinagora"."conta"("id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT ckeck_transferencia check ("tipo" in ('PIX', 'TED', 'DOC'))
 );
 
-CREATE INDEX "fk_transferencia_origem_idx" 
-ON "dindinagora"."transferencia"("origem_fk" ASC);
- 
-CREATE INDEX "fk_transferencia_destino_idx"
-ON "dindinagora"."transferencia"("destino_fk" ASC);
+CREATE INDEX "fk_transferencia_origem_destino_idx"
+ON "dindinagora"."tr_transferencia"("origem_fk","destino_fk");
 
 -- -----------------------------------------------------
 -- Table "dindinagora"."deposito"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "dindinagora"."deposito" (
-  "ID" SERIAL,
+CREATE TABLE IF NOT EXISTS "dindinagora"."tr_deposito" (
+  "id" SERIAL PRIMARY KEY,
   "montante" FLOAT NOT NULL,
   "conta_fk" INT NOT NULL,
-  PRIMARY KEY ("ID"),
+  "data_hora_transacao" TIMESTAMP NOT NULL,
   CONSTRAINT "fk_deposito_conta"
     FOREIGN KEY ("conta_fk")
-    REFERENCES "dindinagora"."conta"("ID")
+    REFERENCES "dindinagora"."conta"("id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
     
 CREATE  INDEX "fk_deposito_cliente_idx"
-ON "dindinagora"."deposito"("conta_fk" ASC);
+ON "dindinagora"."tr_deposito"("conta_fk" ASC);
 
 -- -----------------------------------------------------
 -- Table "dindinagora"."emprestimo"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "dindinagora"."emprestimo" (
-  "ID" SERIAL,
+CREATE TABLE IF NOT EXISTS "dindinagora"."dim_emprestimo" (
+  "id" SERIAL PRIMARY KEY,
   "montante" FLOAT NOT NULL,
   "taxa_aa" FLOAT NOT NULL,
+  "parcelas" INT NOT NULL
+);
+    
+ CREATE INDEX "dim_emprestimo_idx" 
+ ON "dindinagora"."dim_emprestimo"("id" ASC);
+
+ -- -----------------------------------------------------
+-- Table "dindinagora"."emprestimo"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS "dindinagora"."tr_emprestimo" (
+  "id" SERIAL PRIMARY KEY,
   "conta_fk" INT NOT NULL,
-  "parcelas" INT NOT NULL,
-  PRIMARY KEY ("ID"),
+  "emprestimo_fk" INT NOT NULL,
+  "data_hora_emprestimo" TIMESTAMP NOT NULL,
   CONSTRAINT "fk_emprestimo_conta"
-    FOREIGN KEY ("conta_fk")
-    REFERENCES "dindinagora"."conta"("ID")
+  FOREIGN KEY ("conta_fk")
+    REFERENCES "dindinagora"."conta"("id")
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT "fk_emprestimo"
+    FOREIGN KEY ("emprestimo_fk")
+    REFERENCES "dindinagora"."dim_emprestimo"("id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
     
  CREATE INDEX "fk_emprestimo_cliente_idx" 
- ON "dindinagora"."emprestimo"("conta_fk" ASC);
+ ON "dindinagora"."tr_emprestimo"("conta_fk","emprestimo_fk");
--- init-user-db.sh
+++ init-user-db.sh
@@ -4,8 +4,50 @@ set -e
 cp -a home/dados_dindin/. /var/lib/postgresql/data
 
 psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname user <<-EOSQL
+    COPY dindinagora.endereco
+    FROM '/var/lib/postgresql/data/dados_endereco_trat.csv' 
+    DELIMITER ';' 
+    CSV HEADER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname user <<-EOSQL
     COPY dindinagora.cliente
-    FROM '/var/lib/postgresql/data/dados_clientes_3.csv' 
+    FROM '/var/lib/postgresql/data/dados_clientes_trat.csv' 
+    DELIMITER ';' 
+    CSV HEADER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname user <<-EOSQL
+    COPY dindinagora.conta
+    FROM '/var/lib/postgresql/data/dados_conta_trat.csv' 
+    DELIMITER ';' 
+    CSV HEADER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname user <<-EOSQL
+    COPY dindinagora.tr_transferencia
+    FROM '/var/lib/postgresql/data/dados_tr_transferencia_trat.csv' 
+    DELIMITER ';' 
+    CSV HEADER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname user <<-EOSQL
+    COPY dindinagora.tr_deposito
+    FROM '/var/lib/postgresql/data/dados_tr_deposito_trat.csv' 
+    DELIMITER ';' 
+    CSV HEADER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname user <<-EOSQL
+    COPY dindinagora.dim_emprestimo
+    FROM '/var/lib/postgresql/data/dados_dim_emprestimo_trat.csv' 
+    DELIMITER ';' 
+    CSV HEADER;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username $POSTGRES_USER --dbname user <<-EOSQL
+    COPY dindinagora.tr_emprestimo
+    FROM '/var/lib/postgresql/data/dados_tr_emprestimo_trat.csv' 
     DELIMITER ';' 
     CSV HEADER;
 EOSQL
